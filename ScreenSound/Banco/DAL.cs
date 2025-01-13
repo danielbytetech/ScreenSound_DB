2afd3c8bcbe66dab21515d5b4f5b51a3be3ab1aa
43451626888 <43451626888@SISTEMAS-0001>
2025-01-13 11:12:40 -0300

    deixei o DAL totalmente generics

--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -6,11 +6,40 @@ using System.Threading.Tasks;
 
 namespace ScreenSound.Banco
 {
-    abstract class DAL<T>
+    //Where declara para o metodo Set do Entity Framework para que o tipo generico<T> seja/receba uma class
+    class DAL<T> where T : class
     {
-        public abstract IEnumerable<T> Listar();
-        public abstract void Adicionar(T objeto);
-        public abstract void Atualizar(T objeto);
-        public abstract void Deletar(T objeto);
+        protected readonly ScreenSoundContext context;
+
+        public DAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<T> Listar()
+        {
+            return context.Set<T>().ToList();
+        }
+        public void Adicionar(T objeto)
+        {
+            context.Set<T>().Add(objeto);
+            context.SaveChanges();
+        }
+        public void Atualizar(T objeto)
+        {
+            context.Set<T>().Update(objeto);
+            context.SaveChanges();
+        }
+        public void Deletar(T objeto)
+        {
+            context.Set<T>().Remove(objeto);
+            context.SaveChanges();
+        }
+
+        //Func é uma classe onde verifica um tipo de parametro como entrada e retorna um tipo de valor bool 
+        public T? RecuperarPor(Func<T, bool> condicao)
+        {
+            return context.Set<T>().FirstOrDefault(condicao);
+        }
     }
 }
