57c13541ce98df6890f9de2ae9b916d22e001eeb
43451626888 <43451626888@SISTEMAS-0001>
2025-01-16 10:56:41 -0300

    Fiz todas as migrations tanto manual quanto pelo EF

---
+++ ScreenSound/Migrations/20250115133901_AdicionandoMusica.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScreenSound.Migrations
+{
+    public partial class AdicionandoMusica : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Eu te Devoro", 1997 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Walk", 2011 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Na sua estante", 2005 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Você é linda", 2012 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "All My life", 2002 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Teto de Vidro", 2003 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Um amor puro", 1997 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20250116120825_AtualizandoTabela.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScreenSound.Migrations
+{
+    public partial class AtualizandoTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 2 WHERE Id IN (2, 5)");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 3 WHERE Id IN (3, 6)");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 4 WHERE Id = 4");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 7 WHERE Id = 7");
+
+            migrationBuilder.Sql("DELETE FROM Artistas WHERE Id IN (5,6)");            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
